df80d6930e01ac9af8875b390c7be630d674e376
modernNeo <peyvand.manshadi@gmail.com>
2015-10-01 18:30:14 -0700

    First commit

---
+++ fileName.awk
@@ -0,0 +1,29 @@
+#!/usr/bin/awk -f
+BEGIN {
+	out_file = ARGV[2];
+	ARGV[2]="";
+	theName="";
+}
+NF > 8{
+	num_of_recs=NF;
+	while (num_of_recs >= 9){
+		theName=$num_of_recs" "theName;
+		num_of_recs--;
+	}
+	#$7 = month
+	#$6 = day
+	#$8 = time
+	if ($7 == "May"){
+		$7="5"
+	}else if ($7 == "Jun"){
+		$7="6"
+	}else if ($7 == "Jul"){
+		$7="7"
+	}else if ($7 == "Aug"){
+		$7="8"
+	}else if ($7 == "Sep"){
+		$7="9"
+	}
+	print theName"\n"$7" "$6" "$8 >out_file;
+	theName="";
+}
---
+++ syncFoldersOneOff.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+	ls -l ~/Dropbox/Apps/Papyrus\ App/Nexus\ 9/PDFs/1MACM\ 201/ > macmDropFileList.txt
+	ls -l ~/Dropbox/Apps/Papyrus\ App/Nexus\ 9/PDFs/2CMPT\ 276/ > cmptDropFileList.txt
+
+	./fileName.awk macmDropFileList.txt macmDBFileNames.txt
+	./fileName.awk cmptDropFileList.txt cmptDBFileNames.txt
+
+	ls -l ~/Google\ Drive/School\ Files/Current\ Classes/1MACM\ 201/Lecture\ Notes/ > macmGoogleFiles.txt
+	ls -l ~/Google\ Drive/School\ Files/Current\ Classes/2CMPT\ 276/ > cmptGoogleFiles.txt
+
+	./fileName.awk macmGoogleFiles.txt macmGoogleFileNames.txt
+	./fileName.awk cmptGoogleFiles.txt cmptGoogleFileNames.txt
+
+	./compare macmGoogleFileNames.txt macmDBFileNames.txt syncMacm.txt
+	./compare cmptGoogleFileNames.txt cmptDBFileNames.txt syncCmpt.txt
+
+	now="$(date +%y-%m-%d-%T)"
+	printf "last date & time file sync: %s\n" "$now" >> logs/"$now"
+	printf "Files Synced:\n" >> logs/"$now"
+	
+	cat syncMacm.txt | while read line
+	do
+		printf $line"\n" >> logs/"$now"
+		cp ~/"Dropbox/Apps/Papyrus App/Nexus 9/PDFs/1MACM 201/$line" ~/Google\ Drive/School\ Files/Current\ Classes/1MACM\ 201/Lecture\ Notes/
+		rm ~/"Dropbox/Apps/Papyrus App/Nexus 9/PDFs/1MACM 201/$line"
+	done
+
+	cat syncCmpt.txt | while read line
+	do
+		printf $line"\n" >> logs/"$now"
+		cp ~/"Dropbox/Apps/Papyrus App/Nexus 9/PDFs/2CMPT 276/$line" ~/Google\ Drive/School\ Files/Current\ Classes/2CMPT\ 276/
+		rm ~/"Dropbox/Apps/Papyrus App/Nexus 9/PDFs/2CMPT 276/$line"
+	done
+	rm cmptDropFileList.txt macmDropFileList.txt macmDBFileNames.txt cmptDBFileNames.txt macmGoogleFiles.txt cmptGoogleFiles.txt macmGoogleFileNames.txt cmptGoogleFileNames.txt syncMacm.txt syncCmpt.txt
