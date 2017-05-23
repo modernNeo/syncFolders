f5e5a8a9e204616fc8325a8adc5aca1af2206a06
modernNeo <jsaadatm@sfu.ca>
2017-05-23 02:29:12 -0700

    adding source code

---
+++ AppStartermain.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  AppStarter
+//
+//  Created by Jason Manshad on 2015-09-15.
+//  Copyright (c) 2015 Oddment Studios. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    system("cd /Applications/syncFolders/; nohup ./syncFolders.sh &");
+    return 0;
+}
