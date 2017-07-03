17da84feb7561b55c19cbee42ba7344cac06ea86
modernNeo <jason@jason_arch.telus>
2017-07-03 16:04:49 -0700

    changed output of logs to outside the repo

--- ARCH/syncFolders.sh
+++ ARCH/syncFolders.sh
@@ -35,8 +35,8 @@ ls "/home/jason/Google Drive/School/Current Classes/3CMPT 307/Lecture Notes & Sl
 
 
 now="$(date +%y-%m-%d-%T).txt"
-printf "last date & time file sync: %s\n" "$now" >> ../logs/"$now"
-printf "Files Synced:\n" >> ../logs/"$now"
+printf "last date & time file sync: %s\n" "$now" >> ../../logs/"$now"
+printf "Files Synced:\n" >> ../../logs/"$now"
 
 valid=0;
 #./compare philGoogleFileNames.txt philDBFileNames.txt syncPhil.txt
@@ -51,21 +51,21 @@ fi
 if [ $valid == 0 ]; then
 #	cat syncPhil.txt | while read line
 #	do
-#		printf "PHIL 105/$line\n" >> logs/"$now"
+#		printf "PHIL 105/$line\n" >> ../../logs/"$now"
 #		cp ~/"Dropbox/Apps/Papyrus App/Nexus 9/PDFs/1PHIL 105/$line" ~/Google\ Drive/School/Current\ Classes/1PHIL\ 105/Lecture\ Notes/
 #		rm ~/"Dropbox/Apps/Papyrus App/Nexus 9/PDFs/1PHIL 105/$line"
 #	done
 
 	cat sync379.txt | while read line
 	do
-		printf "CMPT 379/$line\n" >> ../logs/"$now"
+		printf "CMPT 379/$line\n" >> ../../logs/"$now"
 		cp ~/"Dropbox/Apps/Papyrus App/Nexus 9/PDFs/2CMPT 379/$line" ~/Google\ Drive/School/Current\ Classes/2CMPT\ 379/Lecture\ Notes/$line
 		rm ~/"Dropbox/Apps/Papyrus App/Nexus 9/PDFs/2CMPT 379/$line"
 	done
 
 	cat sync307.txt | while read line
 	do
-		printf "CMPT 307/$line\n" >> ../logs/"$now"
+		printf "CMPT 307/$line\n" >> ../../logs/"$now"
 		cp ~/"Dropbox/Apps/Papyrus App/Nexus 9/PDFs/3CMPT 307/$line" ~/"Google Drive/School/Current Classes/3CMPT 307/Lecture Notes & Slides/$line"
 		rm ~/"Dropbox/Apps/Papyrus App/Nexus 9/PDFs/3CMPT 307/$line"
 	done
